65fa65ae0c02a24306a178f5ceb1145d03704d45
Chris Tibbs <eggyeggbean@google.com>
2020-11-25 14:04:08 -0500

    Initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Chris Tibbs on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# CS193 Projects
+
+Projects implemented by Chris Tibbs in the [Stanford SwiftUI course](https://cs193p.sites.stanford.edu/).
+
