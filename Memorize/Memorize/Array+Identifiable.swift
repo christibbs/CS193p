5bd8fb738b3ab91c6e374f8dc7d722f9bf04a164
Chris Tibbs <eggyeggbean@google.com>
2020-12-16 16:00:12 -0500

    Use Grid layout

---
+++ Memorize/Memorize/Array+Identifiable.swift
@@ -0,0 +1,19 @@
+//
+//  Array+Identifiable.swift
+//  Memorize
+//
+//  Created by Chris Tibbs on 12/16/20.
+//
+
+import Foundation
+
+extension Array where Element: Identifiable {
+  func firstIndex(matching: Element) -> Int {
+    for index in 0 ..< self.count {
+      if self[index].id == matching.id {
+        return index
+      }
+    }
+    return 0
+  }
+}
